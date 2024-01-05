6ec835412329926727f3ee09a22eaa4db85e29ee
JuanDeTeo <155494254+JuanDeTeo@users.noreply.github.com>
2024-01-05 14:16:33 -0700

    menu admin css avances

--- src/app/admin/menu/page.tsx
+++ src/app/admin/menu/page.tsx
@@ -1,20 +1,28 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.new_style_my_name}>
       <h1>Menu</h1>
       <h3>admin</h3>
+      <br />
 
-      <button>game</button>
-      <br />
-      <button>edit score</button>
-      <br />
-      <button>logout</button>
-      <br />
-      <a href="/login">Login</a>
+      <section className={styles.form_menu}>
+        <button>game</button>
+        <br />
+        <button>edit score</button>
+        <br />
+        <button>logout</button>
+      </section>
 
+      <div className={styles.links}>
+        <p>
+          <a href="/login">Login</a>
+          <br />
+          <a href="/register">Register</a>
+        </p>
+      </div>
     </main>
-  )
+  );
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,22 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.login}>
       <h1>Login</h1>
 
-      <input name="username" placeholder='username' />
-      <br />
-      <input name="password" placeholder='password'/>
-      <br />
-      <button>Login</button>
-      <br />
-      <a href="/register">Creater user</a>
+      <section className={styles.login_user}>
+        <div className={styles.login_input}>
+          <input name="username" placeholder="username" />
+          <br />
+          <input name="password" placeholder="password" />
+        </div>
 
+        <button>Login</button>
+        <br />
+        <a href="/register">Creater user</a>
+      </section>
     </main>
-  )
+  );
 }
